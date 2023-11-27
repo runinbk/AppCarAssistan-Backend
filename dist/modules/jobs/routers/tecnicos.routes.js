6efccbcdc1e1c0c5678cae70ae89ab1800b92ccf
runinbk <runinbk@gmail.com>
2023-11-26 22:25:22 -0400

    add db validators an auth for routes - jobs and services modules

--- dist/modules/jobs/routers/tecnicos.routes.js
+++ dist/modules/jobs/routers/tecnicos.routes.js
@@ -1,47 +1,50 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const express_validator_1 = require("express-validator");
 const tecnicos_controller_1 = require("../controllers/tecnicos.controller");
-// import validarCampos from "../middlewares/validar-campos";
-// import validarJWT from "../middlewares/validar-jwt";
+const validar_campos_1 = __importDefault(require("../../../middlewares/validar-campos"));
+const validar_jwt_1 = __importDefault(require("../../../middlewares/validar-jwt"));
 // import { isAdminRole } from "../middlewares/validar-rol";
 const router = (0, express_1.Router)();
 router.get('/', [
-// validarJWT,
-//  isAdminRole,
-// validarCampos
+    validar_jwt_1.default,
+    //  isAdminRole,
+    validar_campos_1.default
 ], tecnicos_controller_1.gets);
 router.get('/:id', [
     (0, express_validator_1.check)('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validar_jwt_1.default,
     // isAdminRole,
-    // validarCampos
+    validar_campos_1.default
 ], tecnicos_controller_1.get);
 router.post('/', [
-    // validarJWT,
+    validar_jwt_1.default,
     // isAdminRole,
     (0, express_validator_1.check)('rol', 'El rol es obligatorio').not().isEmpty(),
     (0, express_validator_1.check)('descripcion', 'La descripcion es obligatoria').not().isEmpty(),
-    // validarCampos
+    validar_campos_1.default
 ], tecnicos_controller_1.post);
 router.put('/:id', [
     (0, express_validator_1.check)('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validar_jwt_1.default,
     // isAdminRole,
-    // validarCampos
+    validar_campos_1.default
 ], tecnicos_controller_1.put);
 router.delete('/del/:id', [
     (0, express_validator_1.check)('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validar_jwt_1.default,
     // isAdminRole,
-    // validarCampos
+    validar_campos_1.default
 ], tecnicos_controller_1.delet);
 router.delete('/:id', [
     (0, express_validator_1.check)('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validar_jwt_1.default,
     // isAdminRole,
-    // validarCampos
+    validar_campos_1.default
 ], tecnicos_controller_1.deletState);
 exports.default = router;
 //# sourceMappingURL=tecnicos.routes.js.map
--- modules/services/routers/asistencia.routes.ts
+++ modules/services/routers/asistencia.routes.ts
@@ -10,52 +10,52 @@ import {
     deletState
 } from "../controllers/asistencia.controller";
 
-// import validarCampos from "../middlewares/validar-campos";
-// import validarJWT from "../middlewares/validar-jwt";
+import validarCampos from "../../../middlewares/validar-campos";
+import validarJWT from "../../../middlewares/validar-jwt";
 // import { isAdminRole } from "../middlewares/validar-rol";
 
 const router = Router();
 
 router.get('/', [
-    // validarJWT,
+    validarJWT,
     //  isAdminRole,
-    // validarCampos
+    validarCampos
 ], gets);
 
 router.get('/:id', [
     check('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validarJWT,
     // isAdminRole,
-    // validarCampos
+    validarCampos
 ], get);
 
 router.post('/', [
-    // validarJWT,
+    validarJWT,
     // isAdminRole,
     check('rol', 'El rol es obligatorio').not().isEmpty(),
     check('descripcion', 'La descripcion es obligatoria').not().isEmpty(),
-    // validarCampos
+    validarCampos
 ], post);
 
 router.put('/:id', [
     check('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validarJWT,
     // isAdminRole,
-    // validarCampos
+    validarCampos
 ], put);
 
 router.delete('/del/:id', [
     check('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validarJWT,
     // isAdminRole,
-    // validarCampos
+    validarCampos
 ], delet);
 
 router.delete('/:id', [
     check('id').isInt().withMessage('El campo id debe ser un número entero'),
-    // validarJWT,
+    validarJWT,
     // isAdminRole,
-    // validarCampos
+    validarCampos
 ], deletState);
 
 export default router;
